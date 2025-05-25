34c24732ac1c52d4c796727428bb68f731a7213d
daylin <142554060+daylinm@users.noreply.github.com>
2025-05-24 17:53:02 -0700

    added time and start/stop button to task display

--- scenes/task/session/session.gd
+++ scenes/task/session/session.gd
@@ -7,7 +7,6 @@ class_name Session extends Node2D
 @onready var timed_task_scene = preload("res://scenes/task/timed_task/timed_task.tscn")
 
 signal new_day()
-signal time_changed(task: Task, h: int, m: int, s: int)
 
 func _on_child_entered_tree(node: Node) -> void:
 	if not node is Task: return
@@ -15,12 +14,9 @@ func _on_child_entered_tree(node: Node) -> void:
 	node.started.connect(stop_tasks)
 	node.failed.connect(pet.on_task_failed)
 	node.succeeded.connect(pet.on_task_succeeded)
-	node.time_changed.connect(on_task_time_changed)
 	
 	new_day.connect(node.on_new_day)
 
-func on_task_time_changed(task: Task, h: int, m: int, s: int):
-	time_changed.emit(task, h, m, s)
 
 func stop_tasks(started_task: Task):
 	for task in get_children():
